6ac286202adb1aa27a4b0a7bca8b68925e06f56d
weber791024 <80802692+weber791024@users.noreply.github.com>
2023-05-24 21:49:13 +0800

    Update pesqio.c

--- pypesq/pesqio.c
+++ pypesq/pesqio.c
@@ -119,7 +119,6 @@ void make_stereo_file2 (char *stereo_path_name, SIGNAL_INFO *ref_info, float *de
     long            n;
 
     n = ref_info-> Nsamples + DATAPADDING_MSECS  * (Fs / 1000) - 2 * SEARCHBUFFER * Downsample;     
-    printf("%d\n", n);
     buffer = (short *) safe_malloc (2 * n * sizeof (short));
     
     if ((outputFile = fopen (stereo_path_name, "wb")) == NULL) {
@@ -204,7 +203,7 @@ void load_src( long * Error_Flag, char ** Error_Type,
     Fs = fs;
     Nsamples = n_samples;
     sinfo-> Nsamples = Nsamples + 2 * SEARCHBUFFER * Downsample;
-
+    printf("%d\n", sinfo-> Nsamples);
     sinfo-> data =
         (float *) safe_malloc( (sinfo-> Nsamples + DATAPADDING_MSECS  * (Fs / 1000)) * sizeof(float) );
     if( sinfo-> data == NULL )
