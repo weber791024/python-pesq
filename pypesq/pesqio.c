f2d6c312a9ede55b7aad289eb5fc8bf90db0b546
weber791024 <80802692+weber791024@users.noreply.github.com>
2023-06-16 17:50:20 +0800

    Update pesqio.c

--- pypesq/pesqio.c
+++ pypesq/pesqio.c
@@ -223,24 +223,7 @@ void load_src( long * Error_Flag, char ** Error_Type,
 
     int i = Nsamples;
     float dataf=0;
-//     printf("%d\n", i);
-    while(i--){
-          dataf=*p_input;
-          dataf/=32768;
-      *(read_ptr++) = dataf;
-       *(p_input)++;
-//         *(read_ptr++) = float)((*(p_input++));
-    }
-//     printf("%.10f\n", sinfo->data[0]);
-//     printf("%.10f\n", sinfo->data[1]);
-//     printf("%.10f\n", sinfo->data[2]);
   
-//     printf("%d\n", data[0]);
-//     printf("%d\n", data[1]);
-//     printf("%d\n", data[2]);
-    FILE *pFile = fopen("ref_info241io.raw", "wb");
-    fwrite(sinfo->data, sizeof(float), 66880, pFile);
-    fclose(pFile);
     for( read_count = DATAPADDING_MSECS  * (Fs / 1000) + SEARCHBUFFER * Downsample;
          read_count > 0; read_count-- )
       *(read_ptr++) = 0.0f;
