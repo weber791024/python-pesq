8a9dc73ab5ae300947a75b33b37bffa140b861a6
weber791024 <80802692+weber791024@users.noreply.github.com>
2023-06-16 19:54:22 +0800

    Update pesqmain.c

--- pypesq/pesqmain.c
+++ pypesq/pesqmain.c
@@ -329,7 +329,7 @@ void pesq_measure (SIGNAL_INFO * ref_info, SIGNAL_INFO * deg_info,
         }
     
         input_filter( ref_info, deg_info, ftmp );
-        printf("%d",ftmp);
+        printf("%d",&ftmp);
         calc_VAD (ref_info);
         calc_VAD (deg_info);
         
