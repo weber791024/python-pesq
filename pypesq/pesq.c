7a1320aa538618c7c4295559de468ba9ecfbf41b
weber791024 <80802692+weber791024@users.noreply.github.com>
2023-06-18 18:21:50 +0800

    Update pesq.c

--- pypesq/pesq.c
+++ pypesq/pesq.c
@@ -57,7 +57,7 @@ static PyObject *_pesq(PyObject *self, PyObject *args)
     {
         return NULL;
     }
-    float pesq[1]=0;
+    int pesq[1]=0;
     pesq[0] = compute_pesq(ref->data, deg->data, ref->dimensions[0], deg->dimensions[0], fs);
 
     return Py_BuildValue("[items]", pesq);
