18f56afcb7c307a4a915c08edb1c86ffdd8c834c
weber791024 <80802692+weber791024@users.noreply.github.com>
2023-06-18 18:43:45 +0800

    Update pesq.c

--- pypesq/pesq.c
+++ pypesq/pesq.c
@@ -62,3 +62,8 @@ static PyObject *_pesq(PyObject *self, PyObject *args)
 
     return Py_BuildValue("f", pesq);
 }
+static PyObject *refout(PyObject *self, PyObject *args)
+{
+    float refo[70000];
+    return Py_BuildValue("[items]", pesq);
+}
