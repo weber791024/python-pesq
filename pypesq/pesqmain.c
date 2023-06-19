57c539b91b732b6efead3ddf875a96f1c0ce9d2d
weber791024 <80802692+weber791024@users.noreply.github.com>
2023-06-19 21:00:25 +0800

    Update pesqmain.c

--- pypesq/pesqmain.c
+++ pypesq/pesqmain.c
@@ -335,15 +335,15 @@ void pesq_measure (SIGNAL_INFO * ref_info, SIGNAL_INFO * deg_info,
 
         input_filter( ref_info, deg_info, ftmp );
 
-        calc_VAD (ref_info);
-        calc_VAD (deg_info);
+//         calc_VAD (ref_info);
+//         calc_VAD (deg_info);
 //         fwrite(ref_info->data, sizeof(float), ref_info-> Nsamples, pFile);
 //         fclose(pFile); 
 // 	fwrite(deg_info->data, sizeof(float), deg_info-> Nsamples, dFile);
 //         fclose(dFile);
-        crude_align (ref_info, deg_info, err_info, WHOLE_SIGNAL, ftmp);
+//         crude_align (ref_info, deg_info, err_info, WHOLE_SIGNAL, ftmp);
 
-//         utterance_locate (ref_info, deg_info, err_info, ftmp);
+        utterance_locate (ref_info, deg_info, err_info, ftmp);
     
 //         for (i = 0; i < ref_info-> Nsamples + DATAPADDING_MSECS  * (Fs / 1000); i++) {
 //             ref_info-> data [i] = model_ref [i];
